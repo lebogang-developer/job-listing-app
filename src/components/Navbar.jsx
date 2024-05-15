8dc1b02f6f5a1d491b44f2ca78ff5244525f48aa
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-05-15 22:14:14 +0200

    Hero section added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
   return (
@@ -6,14 +6,14 @@ const Navbar = () => {
         <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
           <div className='flex h-20 items-center justify-between'>
             <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-              {/* <!-- Logo --> */}
+             
               <a
                 className='flex flex-shrink-0 items-center mr-4'
                 href='/index.html'
               >
                 <img
                   className='h-10 w-auto'
-                  src='images/logo.png'
+                  src={logo}
                   alt='React Jobs'
                 />
                 <span className='hidden md:block text-white text-2xl font-bold ml-2'>
